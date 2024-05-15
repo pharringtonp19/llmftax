9a533c97282b72e723c7133026ceaea406ab146b
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-05-15 10:28:15 -0400

    Create setup_env.sh

---
+++ getting-started/setup_env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Change directory
+cd llmft
+
+# Name of the virtual environment
+ENV_NAME="llms"
+
+# Create virtual environment
+python3 -m venv $ENV_NAME
+
+# Activate the virtual environment
+source $ENV_NAME/bin/activate
+
+# Install any necessary Python packages
+pip install -U "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip install transformers datasets accelerate peft bitsandbytes ipywidgets evaluate huggingface_hub nvidia-ml-py3 
+pip install matplotlib scikit-learn pytest pre-commit torchtune papermill
+pip install openai tiktoken trl ipykernel anthropic PyPDF2 seaborn
+pip install git+https://github.com/pharringtonp19/trics.git
+pip install flax optax
+
+echo "Setup complete and the virtual environment $ENV_NAME is ready to use."
