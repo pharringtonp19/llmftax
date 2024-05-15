567e19c745a16b8c1455eafde551e68fd4027111
Patrick Power <55798098+pharringtonp19@users.noreply.github.com>
2024-05-15 10:29:04 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llmftax',
+    version='0.1.0',
+    description='Fine Tuing for Causal Inference in Jax',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/pharringtonp19/llmft',  # URL to the repository
+    author='Patrick Power',
+    author_email='your.email@example.com',
+    license='MIT',  # Or whatever license you choose
+    packages=find_packages(),
+    install_requires=[
+        # List your project's dependencies here, e.g.,
+        # 'numpy',
+        # 'pandas',
+    ]
+)
